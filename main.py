1f77ea1e166aa6e3f15302fdad13add819940f12
Dhavamani A <dhavamani17092005@gmail.com>
2026-03-21 21:06:37 +0530

    Add import statement for FewShotPosts

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from few_shot import FewShotPosts
 from post_generator import generate_post
 length_options = ["Short", "Medium", "Long"]
 language_options = ["English", "English"]
+
 def main():
     st.subheader("LinkedIn Post Generator: Codebasics")
     col1, col2, col3 = st.columns(3)
